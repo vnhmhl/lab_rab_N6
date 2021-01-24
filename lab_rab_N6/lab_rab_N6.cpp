28406346a09e66cb36fd4a4d52e7b0d21ab7d59e
IVANOV <79323@DESKTOP-7NSDFUG>
2021-01-24 16:13:17 +0500

    Добавьте файлы проекта.

---
+++ lab_rab_N6/lab_rab_N6.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <string>	
+
+using namespace std;		
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    char* string = new char[255];
+    char* token;
+    char* next_token;
+    unsigned int max = 0;
+    const char* maxStr = "";
+    unsigned int min = 255;
+    const char* minStr =  "";
+
+    cout << "Введите текст:" << endl;
+    gets_s(string, 255);
+
+    token = strtok_s(string, " ", &next_token);
+    while (token != NULL) {
+        if (*token == '.') {
+            token = strtok_s(NULL, " ", &next_token);
+            continue;
+        }
+        if (max < strlen(token)) {
+            maxStr = token;
+            max = strlen(maxStr);
+        }
+        if (min > strlen(token)) {
+            minStr = token;
+            min = strlen(minStr);
+        }
+        token = strtok_s(NULL, " ", &next_token);
+
+    }
+
+    cout << "Самое длинное слово: " << maxStr << endl;
+    cout << "Самое короткое слово: " << minStr << endl;
+    system("pause");
+    return 0;
+
+}
